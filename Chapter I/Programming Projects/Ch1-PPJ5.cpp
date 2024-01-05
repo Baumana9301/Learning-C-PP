8e4e4bba6fb376d528e6673b700437b7243c9b1b
Baumana9301 <baumana9301@gmail.com>
2024-01-05 16:38:23 -0500

    Chapter I Project V - User Input Letter C

---
+++ Chapter I/Programming Projects/Ch1-PPJ5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char user_character;
+
+    cout << "Please enter a character to be used:\n";
+    cin >> user_character;
+    cout << "  ";
+    cout << user_character;
+    cout << " ";
+    cout << user_character;
+    cout << " ";
+    cout << user_character;
+    cout << "\n";
+    cout << " ";
+    cout << user_character;
+    cout << "    ";
+    cout << user_character;
+    cout << "\n";
+    cout << user_character;
+    cout << "\n";
+    cout << user_character;
+    cout << "\n";
+    cout << user_character;
+    cout << "\n";
+    cout << user_character;
+    cout << "\n";
+    cout << user_character;
+    cout << "\n";
+    cout << " ";
+    cout << user_character;
+    cout << "    ";
+    cout << user_character;
+    cout << "\n";
+    cout << "  ";
+    cout << user_character;
+    cout << " ";
+    cout << user_character;
+    cout << " ";
+    cout << user_character;
+    cout << "\n";
+
+    return 0;
+}
