0529bdad38e65543861b6b8d8e518fa4ab67bd97
Baumana9301 <baumana9301@gmail.com>
2024-01-05 11:32:27 -0500

    Ch1 Practice Problem I - Favorite Number

---
+++ Ch1-PP1-Favorite_Number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int favorite_number;
+
+    cout << "Please enter your favorite number:\n";
+    cin >> favorite_number;
+    cout << "Your favorite number is: ";
+    cout << favorite_number;
+
+    return 0;
+}
