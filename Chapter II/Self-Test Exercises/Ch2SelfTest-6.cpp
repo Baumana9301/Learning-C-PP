ea9fc73175d2be200b1d2906fefd7916d689d742
Baumana9301 <baumana9301@gmail.com>
2024-01-06 10:14:18 -0500

    Chapter II - 2.1 Self-test VI - Garbage Values

---
+++ Chapter II/Self-Test Exercises/Ch2SelfTest-6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3;
+    double n4, n5, n6;
+
+    cout << "The value of n1 is: " << n1;
+    cout << "The value of n2 is: " << n2;
+    cout << "The value of n3 is: " << n3;
+    cout << "The value of n4 is: " << n4;
+    cout << "The value of n5 is: " << n5;
+    cout << "The value of n6 is: " << n6;
+
+    return 0;
+}
