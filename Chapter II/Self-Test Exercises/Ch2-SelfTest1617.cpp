5b31d6dbdad294d67fc69550c99f9529500583f4
Baumana9301 <baumana9301@gmail.com>
2024-01-07 18:47:21 -0500

    Chapter II // SelfTest 16 & 17

---
+++ Chapter II/Self-Test Exercises/Ch2-SelfTest1617.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a, b, c;
+    int number;
+
+    a = 'b';
+    b = 'c';
+    c = a;
+
+    cout << a << b << c << 'c';
+
+    number = (1/3) * 3;
+    
+    cout << "\n(1/3) * 3 is equal to " << number << endl;
+
+    return 0;
+}
