2b3dd57d1127da8b4995a70fdc481c2840cb19ed
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-14 20:29:15 -0500

    Chapter III - SelfTest20 - Local Variables & Blocks

---
+++ Chapter III/Self-Test Exercises/Ch3-SelfTest20.cpp
@@ -0,0 +1,23 @@
+/*Chapter III - SelfTest20 - 
+Local Variables & Nested Blocks*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    cout << x << endl;
+    {
+        cout << x << endl;
+        int x = 2;
+        cout << x << endl;
+        {
+            cout << x << endl;
+            int x = 3;
+            cout << x << endl;
+        }
+        cout << x << endl;
+    }
+    cout << x << endl;
+}
